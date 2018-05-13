0b14022ca0827d9a7681f16ba7698b95c8054343
Trevor Strieber <trevor@strieber.org>
2018-05-12 18:38:07 -0700

    Basic shading.

--- sphere.go
+++ sphere.go
@@ -1,11 +1,15 @@
 package main
 
+import (
+	"math"
+)
+
 type Sphere struct {
 	center Vec3
 	radius float64
 }
 
-func (s Sphere) didHit(r Ray) bool {
+func (s Sphere) didHit(r Ray) float64 {
 	oc := r.origin().subtract(s.center)
 
 	a := r.direction().dot(r.direction())
@@ -14,17 +18,25 @@ func (s Sphere) didHit(r Ray) bool {
 
 	discriminant := b*b - 4*a*c
 
-	return discriminant > 0
+	if discriminant < 0 {
+		return -1.0
+	}
+
+	return (-b - math.Sqrt(discriminant)) / (2.0 * a)
 }
 
 func (s Sphere) colorAt(r Ray) Vec3 {
-	if s.didHit(r) {
-		return Vec3{1.0, 0.0, 0.0}
+	t := s.didHit(r)
+
+	if t > 0.0 {
+		N := r.pointAtParameter(t).subtract(Vec3{0, 0, -1}).unitVector()
+
+		return Vec3{N.x() + 1, N.y() + 1, N.z() + 1}.multiplyScalar(0.5)
 	}
 
 	unitDirection := r.direction().unitVector()
 
-	t := 0.5 * (unitDirection.y() + 1.0)
+	t = 0.5 * (unitDirection.y() + 1.0)
 
 	v1 := Vec3{1.0, 1.0, 1.0}.multiplyScalar(1.0 - t)
 	v2 := Vec3{0.5, 0.7, 1.0}.multiplyScalar(t)
